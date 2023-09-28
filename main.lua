9077a3c875b47e011d8b7e2c5638fa80a549f4c2
Xarix <93476338+Xari-x@users.noreply.github.com>
2023-09-28 10:49:29 +0200

    added loadTilemap.lua and edited main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,17 @@
 _G.love = require("love")
 
 function love.draw()
+    -- Draw the map and all objects within
+	love.graphics.setColor(1, 1, 1)
+	map:draw()
+
+	-- Draw Collision Map (useful for debugging)
+	love.graphics.setColor(1, 0, 0)
+	map:box2d_draw()
+
+	-- Please note that map:draw, map:box2d_draw, and map:bump_draw take
+	-- translate and scale arguments (tx, ty, sx, sy) for when you want to
+	-- grow, shrink, or reposition your map on screen.
+
     love.graphics.circle("fill",50,50,50,5)
 end
